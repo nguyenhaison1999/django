67a7bcdd32df838e8647292d4ee95966fa0fbc52
Nguyen Hai-Son <nhson17@apcs.vn>
2020-12-25 15:00:55 +0700

    New

--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -1,11 +1,22 @@
 from .models import Lead
 from rest_framework import viewsets, permissions
 from .serializers import LeadSerializer
-
+from django.core.exceptions import PermissionDenied
 # Lead Viewset
+
+
 class LeadViewSet(viewsets.ModelViewSet):
-    queryset = Lead.objects.all()
     permissions_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated
     ]
+
     serializer_class = LeadSerializer
+
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            raise PermissionDenied()
+
+        return self.request.user.leads.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
