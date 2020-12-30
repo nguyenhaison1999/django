d6123485db610e7fafa5960c06e018cecca6aba4
Nguyen Hai-Son <nhson17@apcs.vn>
2020-12-30 19:56:03 +0700

    Deep Learning in Django || (Image Classification) || Complete tutorial 2020

---
+++ django_ml_img_class/imageNetProj/firstApp/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+from keras.models import load_model
+from keras.preprocessing import image
+import json
+import tensorflow as tf
+from tensorflow import Graph
+import os
+# Create your views here.
+
+img_height, img_width = 224, 224
+with open('./models/imagenet_classes.json', 'r') as f:
+    labelInfo = f.read()
+
+labelInfo = json.loads(labelInfo)
+
+model_graph = Graph()
+with model_graph.as_default():
+    tf_session = tf.compat.v1.Session()
+    with tf_session.as_default():
+        model = load_model('./models/MobileNetModelImagenet.h5')
+
+
+def index(request):
+    context = {'a': 1}
+    return render(request, 'index.html', context)
+
+
+def predictImage(request):
+    fileObj = request.FILES['filePath']
+    fs = FileSystemStorage()
+    filePathName = fs.save(fileObj.name, fileObj)
+    filePathName = fs.url(filePathName)
+    testimage = '.' + filePathName
+    img = image.load_img(testimage, target_size=(img_height, img_width))
+    x = image.img_to_array(img)
+    x = x/255
+    x = x.reshape(1, img_height, img_width, 3)
+    with model_graph.as_default():
+        with tf_session.as_default():
+            predict = model.predict(x)
+
+    import numpy as np
+    predictedLabel = labelInfo[str(np.argmax(predict[0]))]
+
+    context = {'filePathName': filePathName,
+               'predictedLabel': predictedLabel[1]}
+    return render(request, 'index.html', context)
+
+
+def viewDataBase(request):
+    listOfImages = os.listdir('./media/')
+    listOfImagesPath = ['./media/' + i for i in listOfImages]
+    context = {'listOfImagesPath': listOfImagesPath}
+    return render(request, 'viewDB.html', context)
