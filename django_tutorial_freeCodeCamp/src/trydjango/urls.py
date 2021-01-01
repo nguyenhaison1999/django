e4ce424f2dab13c8158201d7b27db8314fb067a5
Nguyen Hai-Son <nhson17@apcs.vn>
2021-01-01 16:38:05 +0700

    freeCodeCapm-Django-1:26:48

---
+++ django_tutorial_freeCodeCamp/src/trydjango/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from pages.views import home_view
+
+urlpatterns = [
+    path('', home_view, name='home'),
+    path('admin/', admin.site.urls),
+]
