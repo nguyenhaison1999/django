c5b2eaa0de1c56f3e54d38b0444d294705b2ae23
Nguyen Hai-Son <nhson17@apcs.vn>
2021-01-02 20:13:07 +0700

    2:06:49 - Hơ Django Templates Load with Apps

--- django_tutorial_freeCodeCamp/src/pages/views.py
+++ django_tutorial_freeCodeCamp/src/pages/views.py
@@ -6,4 +6,5 @@ from django.http import HttpResponse
 
 def home_view(request, *args, **kwargs):
     # return HttpResponse("<h1>Hello World</h1>")
-    return render(request, "home.html", {})
+    context = {"test": "Fuck you!", "list": [123, 321, 213, 312]}
+    return render(request, "home.html", context)
--- django_tutorial_freeCodeCamp/src/trydjango/urls.py
+++ django_tutorial_freeCodeCamp/src/trydjango/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view
+from products.views import product_detail_view
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('product/', product_detail_view),
     path('admin/', admin.site.urls),
 ]
