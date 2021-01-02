ac3615eed32eca78a414706aead20e945ef3177d
Nguyen Hai-Son <nhson17@apcs.vn>
2021-01-03 01:16:49 +0700

    Add requirements.txt for djangoFreeCodeCamp

--- django_tutorial_freeCodeCamp/src/products/models.py
+++ django_tutorial_freeCodeCamp/src/products/models.py
@@ -7,4 +7,4 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=1000)
-    summary = models.TextField()
+    summary = models.TextField(default=False)
