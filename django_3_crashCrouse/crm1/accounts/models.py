86e8dc158c562619facafbdb11c43336ae7a0e83
named <named@nameds-MacBook-Pro.local>
2021-01-08 01:00:28 +0700

    django_3_video_11

--- django_3_crashCrouse/crm1/accounts/models.py
+++ django_3_crashCrouse/crm1/accounts/models.py
@@ -5,9 +5,8 @@ from django.db import models
 class Customer(models.Model):
 	name = models.CharField(max_length=200, null=True)
 	phone = models.CharField(max_length=200, null=True)
-	email = models.CharField(max_length=200, null=True)
+	email = models.EmailField(max_length=200, null=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
-
 
 	def __str__(self):
 		return self.name
@@ -26,7 +25,7 @@ class Product(models.Model):
 			) 
 
 	name = models.CharField(max_length=200, null=True)
-	price = models.FloatField(null=True)
+	price = models.DecimalField(decimal_places=2, max_digits=100000 , null=True)
 	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
 	description = models.CharField(max_length=200, null=True, blank=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
@@ -34,9 +33,6 @@ class Product(models.Model):
 
 	def __str__(self):
 		return self.name
-
-
-
 
 class Order(models.Model):
 	STATUS = (
@@ -49,5 +45,11 @@ class Order(models.Model):
 	product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 	status = models.CharField(max_length=200, null=True, choices=STATUS)
+
+	def __str__(self):
+		return self.product.name
+
+
+
 
 	
