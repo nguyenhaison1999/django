2daa5e47e8e0b0b8f299378e983628e1c8119679
Nguyen Hai-Son <nhson17@apcs.vn>
2021-01-12 00:30:16 +0700

    Image File Upload to User Profile Model

--- django_3_crashCrouse/crm1/accounts/views.py
+++ django_3_crashCrouse/crm1/accounts/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth.decorators import login_required
 from .models import *
 from .decorators import unauthenticated_user, allowed_users, admin_only
 from .filters import OrderFilter
-from .forms import OrderForm,  CreateUserForm
+from .forms import *
 # Create your views here.
 
 
@@ -93,6 +93,20 @@ def userPage(request):
     context = {'orders': orders, 'total_orders': total_orders, 'delivered': delivered,
                'pending': pending}
     return render(request, 'accounts/user.html', context)
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
+def accountSettings(request):
+    customer = request.user.customer
+    form = CustomerForm(instance=customer)
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, request.FILES, instance=customer)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+    return render(request, 'accounts/account_settings.html', context)
 
 
 @login_required(login_url='login')
